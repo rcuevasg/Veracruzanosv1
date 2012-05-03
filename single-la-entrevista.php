8c8648a67cc198344ce3cdc1bd4c7a66a05c0eb5
Florencia Rosenfeld <frosenfeld@gmail.com>
2012-05-02 20:42:33 -0500

    CSS y estructura single entrevista

--- single-la-entrevista.php
+++ single-la-entrevista.php
@@ -1,4 +1,4 @@
-<div id="content-post">
+<div id="content-post" class="entrevista">
 	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	
@@ -8,26 +8,51 @@
 		</div><!-- .entry-meta -->
 	
 		<div class="entry-content">
-			<div class="la-entrevista">
+		
+		
+			<div class="img">
+	 			
+	 			<?php //Obtenemos la url de la imagen destacada
+					$domsxe = simplexml_load_string(get_the_post_thumbnail($post->ID, 'big'));
+					$thumbnailsrc = "";
+					if (!empty($domsxe))
+						$thumbnailsrc = $domsxe->attributes()->src;
+					if (!empty($thumbnailsrc)):
+				?>
+		 			<img src='<?php bloginfo('template_url') ?>/timthumb.php?src=<?php print $thumbnailsrc; ?>&w=378&h=240' border=0 />
+	 			<?php
+	 			endif;
+	 			?>
+						 			
+			</div> <!-- Fin del div img -->
+
+			<div class="video">
 				<?php
-				the_post_thumbnail('medium');
 				$urlVideo = get_post_meta($post->ID, 'video', true);
 				//$urlVideo = substr(get_the_excerpt(), 0, strrpos(get_the_excerpt(), "<a")-1);
 				global $smart_youtube_pro;
 				print $smart_youtube_pro->check( $urlVideo, 0);
 				?>
 			</div> <!-- Fin del div video -->
-			<span>Semblanza del personaje</span>
-			<?php print substr(get_the_excerpt(), 0, strrpos(get_the_excerpt(), "<a")-1); ?>
-			<span>La entrevista</span>
-			<?php the_content(); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'coraline' ), 'after' => '</div>' ) ); ?>
+			
+			<div class="box-add">
+				<!--aqui deberia ir compartir y relacionados -->	 
+			</div>
+			
+			<div class="bio">
+				<span class="subtitle">Semblanza del personaje</span>
+				<?php print substr(get_the_excerpt(), 0, strrpos(get_the_excerpt(), "<a")-1); ?>
+			</div>
+			
+			<span class="subtitle">La entrevista</span>
+				<?php the_content(); ?>
+				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'coraline' ), 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-content -->
 	
 	</div><!-- #post-## -->
 	
 		<div id="title-comments">
-					<h3>COMENTARIOS</h3>
+			<h3>COMENTARIOS</h3>
 	    </div>
 		
 	<?php comments_template( '', true ); ?>
