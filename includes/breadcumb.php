4a4af1cb48788f3de28e54111492a75eac5f460f
Raul Cuevas Gorocica <raul8527@gmail.com>
2012-05-03 22:30:19 -0500

    breadcumbs corregido nuevamente

--- includes/breadcumb.php
+++ includes/breadcumb.php
@@ -1,8 +1,16 @@
 <div id="breadcrumbs">
 				<?php 
 				//print "El id actual es " . $cat_ID;
-				$category = get_the_category();
-				$idCategoria = get_cat_id( single_cat_title("",false) );//$category[0]->cat_ID;
+				
+				$idCategoria = 0;
+				$muestraCompleto = false;
+				if (is_page() || is_single()):
+					$category = get_the_category();
+					$idCategoria = $category[0]->cat_ID;
+					$muestraCompleto = true;
+				else:
+					$idCategoria = get_cat_id( single_cat_title("",false) );//$category[0]->cat_ID;
+				endif;
 				//$idCategoria = get_cat_ID(single_cat_title( '', false ));
 				$navegacion = get_category_parents($idCategoria,true,',',true); 
 				$elementosNavegacion = explode(",",$navegacion);
@@ -15,7 +23,11 @@
 					foreach ($elementosNavegacion as $item){
 						if (!empty($item)):
 							if ($elemMenu == $numElementos):
-								print " / ";
+								if ($muestraCompleto):
+									print " / " . $item;
+								else:
+									print " / ";
+								endif;
 								//continue;
 							else:
 								print " / " . $item;
@@ -32,7 +44,11 @@
 					foreach ($elementosNavegacion as $item){
 						if (!empty($item)):
 							if ($elemMenu == $numElementos):
-								print " / ";
+								if ($muestraCompleto):
+									print " / " . $item;
+								else:
+									print " / ";
+								endif;
 								//continue;
 							else:
 								print " / " . $item;
